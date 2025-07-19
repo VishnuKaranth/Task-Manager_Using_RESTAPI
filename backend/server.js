b58b0f7afae336fa10a7a96898bee807afcc1a9c
Vishnu <vishnukaranth04@gmail.com>
2025-07-19 19:57:01 +0530

    Final delpoy

--- backend/server.js
+++ backend/server.js
@@ -9,17 +9,18 @@ const connectDB = require('./config/db');
 connectDB();
 
 const app = express();
-app.use(cors({
-  origin: 'https://task-manager-using-restapi.vercel.app' 
-}));
 
 // Middleware
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://task-manager-using-restapi.vercel.app');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
+// --- Start of CORS Configuration ---
+const corsOptions = {
+  origin: 'https://task-manager-using-restapi.vercel.app',
+  methods: 'GET,POST,PUT,DELETE',
+  allowedHeaders: 'Content-Type,Authorization',
+  optionsSuccessStatus: 200 // For legacy browser support
+}
+app.use(cors(corsOptions));
+// --- End of CORS Configuration ---
+
 app.use(express.json());
 app.use(morgan('dev'));
 
